859152230756ba27c12523203e9317296438c0c3
Sr. Garcia <116229277+Mchel007@users.noreply.github.com>
2022-10-20 01:19:35 +0000

    aula 1

--- main.py
+++ main.py
@@ -8,4 +8,11 @@ nome = "Michel"
 idade = 20
 
 #exibir o nome que esta dentro de uma variavel
-print(nome, idade)
+print(nome)
+#quando quiser exibir a farse minah idade é, completando com o conteudo da idade
+print ("minha idade é "+str(idade)+" anos" )
+print (f"minha idade é {idade} anos ")
+print ( "minha idade é {} anos".format (idade))
+
+#quando quiser exibir "meu nome é... e tenho... anos..." trocando pelas variavies nome e idade
+print ("meu nome é {} e tenho {} anos".format(nome, idade))
