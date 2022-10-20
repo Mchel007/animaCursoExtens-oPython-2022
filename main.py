b0bfe78e75c7fd213e7718f567142ca7597eed29
Sr. Garcia <116229277+Mchel007@users.noreply.github.com>
2022-10-20 00:53:07 +0000

    aprendendo variavies

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 #
 #print () = comando de saida
 print ("alo mundo!")
+
+#quiser guardar string | frase
+nome = "Michel"
+idade = 20
+
+#exibir o nome que esta dentro de uma variavel
+print(nome, idade)
