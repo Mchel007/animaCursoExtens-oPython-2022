ac74ea28d8a9255dd842a7d841a30f2d692686fd
Sr. Garcia <116229277+Mchel007@users.noreply.github.com>
2022-11-10 01:02:55 +0000

    funções+lista+laço

--- aula3-2022-11-09b.py
+++ aula3-2022-11-09b.py
@@ -12,13 +12,20 @@ print (imposto2 )
 
 #criar uma funçao calcular_imposto() que calcula um imposto de 5% e retorna
 def calcular_imposto(preco_produto):
-  imposto = preco_produto * 0.05
+  imposto = preco_produto * 0.07
   return imposto
 
 #calculando imposto:
 preco = 299
 imposto = calcular_imposto(preco)
-print(imposto)
+print(f"essa funcao é (7%):{imposto}")
+
+#imposto agr 7% ()
+
+valores = [1.99, 24.50, 78.27, 1515.5]
+#imposto desses quatro valores
+for valor in valores:
+  print (f"o imposto de {valor} é {calcular_imposto(valor)}")
 
   
 
