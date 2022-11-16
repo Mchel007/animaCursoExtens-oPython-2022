c138e09b72ce3bd74f35b440f3fd78af1548fc54
Sr. Garcia <116229277+Mchel007@users.noreply.github.com>
2022-11-16 23:33:11 +0000

    aula4 - acessando informaçoes no banco de dados

---
+++ aula4_2022-11-16a.py
@@ -0,0 +1,27 @@
+#importar biblioteca sqlite3 ( )
+import sqlite3
+
+#vamos estabelecer uma conexao com o banco de dados
+conexao = sqlite3.connect("dc_universe (1).db")
+
+#criar um objeto do tipo cursor()
+cursor = conexao.cursor()
+
+#comando SQL do banco
+sql = "SELECT pessoa_id, nome, nome_civil, tipo FROM pessoas"
+
+#executar o comando sqp no sqlite (no cursor) ()
+cursor.execute(sql)
+
+#exibir a consuta com todos os nomes de herois e viloes()
+pessoas = cursor.fetchall()
+for pessoa in pessoas:
+  print(pessoa)
+
+for pessoa in pessoas:
+  print(f"nome: {pessoa[1]} ({pessoa[3]})")
+                
+
+
+
+
